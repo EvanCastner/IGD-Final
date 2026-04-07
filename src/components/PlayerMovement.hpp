3c645ecfd92ce503c661ca495323e57a089a583b
Evan <ekc30@uakron.edu>
2026-04-06 21:40:30 -0400

    Add PlayerMovement class

---
+++ src/components/PlayerMovement.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../core/Component.hpp"
+#include "../core/Entity.hpp"
+#include "Transform.hpp"
+#include <SDL2/SDL.h>
+
+class PlayerMovement : public Component
+{
+public:
+    float speed = 200.0f;
+
+    void Update(float deltaTime) override
+    {
+        const Uint8 *keys = SDL_GetKeyboardState(NULL);
+
+        Transform *transform = owner->AddComponent<Transform>(); // TEMP
+
+        if (keys[SDL_SCANCODE_W])
+            transform->y -= speed * deltaTime;
+        if (keys[SDL_SCANCODE_S])
+            transform->y += speed * deltaTime;
+        if (keys[SDL_SCANCODE_A])
+            transform->x -= speed * deltaTime;
+        if (keys[SDL_SCANCODE_D])
+            transform->x += speed * deltaTime;
+    }
+};
