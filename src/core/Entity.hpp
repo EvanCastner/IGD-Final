ca3a5fdcfe51f059273207db145c0bb9a3ff83b7
Evan <ekc30@uakron.edu>
2026-04-06 21:16:39 -0400

    Add Entity object

---
+++ src/core/Entity.hpp
@@ -0,0 +1,27 @@
+#include "Component.hpp"
+#include <vector>
+#include <memory>
+
+class Entity
+{
+private:
+    std::vector<std::unique_ptr<Component>> components;
+
+public:
+    template <typename T, typename... Args>
+    T *AddComponent(Args &&...args)
+    {
+        T *component = new T(std::forward<Args>(args)...);
+        component->owner = this;
+        components.emplace_back(component);
+        return component;
+    }
+
+    void Update(float deltaTime)
+    {
+        for (auto &comp : components)
+        {
+            comp->Update(deltaTime);
+        }
+    }
+};
